34657f9d4fb9f145a87815caa80268665cb891c9
sayeef-rahman <sayeef.cse@gmail.com>
2022-12-19 23:17:25 +0600

    code completd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Papa from "papaparse";
 
 function App() {
+  const [convertedRawData, setConvertedRawData] = useState([]);
+  console.log("convertedRawData: ", convertedRawData);
+  const keys = convertedRawData[0];
+  const values = convertedRawData.slice(1, convertedRawData.length);
+  function createObjects(keys, values) {
+    let result = [];
+    for (let i = 0; i < values.length; i++) {
+      let obj = {};
+      for (let j = 0; j < keys.length; j++) {
+        obj[keys[j]] = values[i][j];
+      }
+      result.push(obj);
+    }
+    return result;
+  }
+  const convertedObject = createObjects(keys, values);
+  console.log("convertedObject:", convertedObject);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        accept=".csv,.xlsx,.xls"
+        onChange={(e) => {
+          const files = e.target.files;
+          if (files) {
+            console.log(files[0]);
+            Papa.parse(files[0], {
+              complete: function (results) {
+                setConvertedRawData(results.data);
+                console.log("Finished:", results.data);
+              },
+            });
+          }
+        }}
+      />
     </div>
   );
 }
